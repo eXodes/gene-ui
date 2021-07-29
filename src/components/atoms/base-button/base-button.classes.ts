57f5dd92e9dc3ba1b393c99a4de567f5c04a8db8
Exo Des <hello@exodes.net>
2021-07-30 04:29:50 +0800

    feat(atoms): add base-button.classes.ts

---
+++ src/components/atoms/base-button/base-button.classes.ts
@@ -0,0 +1,39 @@
+export const classNames = {
+  appearance: {
+    fill: {
+      default:
+        "g-bg-gray-600 g-text-clear hover:g-bg-gray-500 focus:g-ring-gray-600 dark:g-bg-gray-100 dark:g-text-gray-700 dark:hover:g-bg-gray-300 dark:focus:g-ring-gray-600",
+      primary:
+        "g-bg-primary-600 g-text-clear hover:g-bg-primary-500 focus:g-ring-primary-600",
+      attention:
+        "g-bg-attention-600 g-text-clear hover:g-bg-attention-500 focus:g-ring-attention-600",
+      success:
+        "g-bg-success-600 g-text-clear hover:g-bg-success-500 focus:g-ring-success-600",
+      warning:
+        "g-bg-warning-600 g-text-clear hover:g-bg-warning-500 focus:g-ring-warning-600",
+      danger:
+        "g-bg-danger-600 g-text-clear hover:g-bg-danger-500 focus:g-ring-danger-600",
+      disabled: "g-bg-gray-200 g-text-gray-400 g-cursor-not-allowed",
+    },
+    clear: {
+      default:
+        "g-bg-transparent g-text-gray-600 hover:g-bg-gray-100 focus:g-ring-gray-600 dark:g-text-gray-100 dark:hover:g-bg-gray-800",
+      primary:
+        "g-bg-transparent g-text-primary-600 hover:g-bg-primary-100 focus:g-ring-primary-600 dark:hover:g-bg-primary-800 dark:hover:g-bg-opacity-25",
+      attention:
+        "g-bg-transparent g-text-attention-600 hover:g-bg-attention-100 focus:g-ring-attention-600 dark:hover:g-bg-attention-800 dark:hover:g-bg-opacity-25",
+      success:
+        "g-bg-transparent g-text-success-600 hover:g-bg-success-100 focus:g-ring-success-600 dark:hover:g-bg-success-800 dark:hover:g-bg-opacity-25",
+      warning:
+        "g-bg-transparent g-text-warning-600 hover:g-bg-warning-100 focus:g-ring-warning-600 dark:hover:g-bg-warning-800 dark:hover:g-bg-opacity-25",
+      danger:
+        "g-bg-transparent g-text-danger-600 hover:g-bg-danger-100 focus:g-ring-danger-600 dark:hover:g-bg-danger-800 dark:hover:g-bg-opacity-25",
+      disabled: "g-bg-transparent g-text-gray-400 g-cursor-not-allowed",
+    },
+  },
+  size: {
+    small: "g-py-1 g-px-2 g-text-xs focus:g-ring-2",
+    medium: "g-py-1.5 g-px-3 g-text-sm focus:g-ring-4",
+    large: "g-py-2 g-px-4 g-text-md focus:g-ring-4",
+  },
+};
