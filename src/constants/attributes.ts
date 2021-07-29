652f983d22d3732bad01db71915120b84455dff2
Exo Des <hello@exodes.net>
2021-07-30 04:28:58 +0800

    feat: add attributes.ts constant

---
+++ src/constants/attributes.ts
@@ -0,0 +1,19 @@
+export enum APPEARANCE {
+  DEFAULT = "default",
+  PRIMARY = "primary",
+  ATTENTION = "attention",
+  SUCCESS = "success",
+  WARNING = "warning",
+  DANGER = "danger",
+}
+
+export enum SIZE {
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+}
+
+export enum VARIANT {
+  FILL = "fill",
+  CLEAR = "clear",
+}
